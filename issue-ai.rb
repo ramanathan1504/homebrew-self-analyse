4754b26b32359f9b0b7ddd7da1f6690f37881201
Ramanathan <ramanathanbscmca@gmail.com>
2026-06-11 00:32:12 +0530

    Bump Homebrew version to v1.0.4

--- issue-ai.rb
+++ issue-ai.rb
@@ -1,8 +1,8 @@
 class IssueAi < Formula
   desc "Offline-first, AI-powered Personal Copilot for Open Source Triage"
   homepage "https://github.com/ramanathan1504/issue-ai"
-  url "https://github.com/ramanathan1504/issue-ai/releases/download/v1.0.3/issue-ai-1.0.3.jar"
-  sha256 "61f1c2ca846b74b95a99a50dbf2a2ff8848876efd43d0db6916d196c7dc2024e"
+  url "https://github.com/ramanathan1504/issue-ai/releases/download/v1.0.4/issue-ai-1.0.4.jar"
+  sha256 "c2ae7e61066f25d36e9280000164411b8e3b29e0b5764dec974123bc4f9eeaed"
   license "Apache-2.0"
 
   depends_on "openjdk@17"
